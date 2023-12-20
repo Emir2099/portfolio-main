fdcda87b75fc54b3d2fa3bcd6fe184828a3e496f
Emir || 2099 <emirsteveroger@gmail.com>
2023-12-20 15:40:39 +0530

    new

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,8 +3,8 @@ import CV from '../../assets/emir_husain_resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        {/* <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a> */}
+        <a href={CV} download className='btn'>Download CV</a>
+        <a href="https://www.linkedin.com/in/emir-husain-79555624a/" className='btn btn-primary'>Let's Talk</a> 
     </div>
   )
 }
