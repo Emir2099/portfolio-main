c3e58e581928ea171a5d31225ddfe1c28f158bfc
Emir || 2099 <emirsteveroger@gmail.com>
2024-01-04 13:34:24 +0530

    img change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/img1_main.png'
+import ME from '../../assets/img_e.png'
 import HeaderSocial from './HeaderSocials'
 
 function Header() {
